bc834b14d8589fb956853550503b35eff279dca3
Julius Rüberg <julius.rueberg@protonmail.ch>
2023-06-11 16:55:00 +0200

    indent some command outputs

--- backup_btrfs_timeshift.sh
+++ backup_btrfs_timeshift.sh
@@ -25,7 +25,7 @@ case $i in
         echo "--force-mount                     Forces script to mount <DEST> first"
         echo "--no-delete                       Does not sync deletion of snapshot at <ROOT>. Does delete obsolute readonly subvolume at <ROOT>"
         echo "-q,--quiet                        Supresses output to a minimum"
-        echo "--subvol=@<subv>                  Only backups the specified subvolume"
+        echo "--subvol=@<subv>                  Only backup specified subvolume"
         #echo "--dry-run                         Will only simulate backup process without altering any files" # WIP
         exit 0
         shift
@@ -76,12 +76,13 @@ case $i in
     	;;
 esac
 done
-if [ "$(id -u)" -ne 0 ]; then
-        echo "ERROR This utility needs to run as root to create btrfs subvolumes!"
-        exit 1
-fi
+
 
 ## HELPERS ##
+
+indent() {
+    sed 's/^/  /'
+}
 
 log() {
     [ ! $QUIET ] && echo "$@"
@@ -103,13 +104,18 @@ err() {
     exit 1
 }
 
+# check for privileges first
+if [ "$(id -u)" -ne 0 ]; then
+        err "This utility needs to run as root to create btrfs subvolumes!"
+fi
+
 function umount_dest() {
     if [ $MOUNT ]
     then
-        logv "Unmount and removing created mountpoint..."
+        logv "Unmount and removing created mountpoint"
         sleep 3
         umount "$SYNC_DEST" && rmdir "$SYNC_DEST"
-        echo "Finished."
+        log "Finished."
     fi
 }
 
@@ -121,7 +127,7 @@ function snapshot_deleted() {
 function backup_incremental() {
     CRITICAL="$3/$(basename $2)"
     [ $DEBUG ] && set -x
-    btrfs $(logvv "-v") send -p "$1" "$2" | btrfs $(logvv "-v") receive "$3"
+    btrfs $(logvv "-v") send -p "$1" "$2" | btrfs $(logvv "-v") receive "$3" # todo indent output
     [ $DEBUG ] && set +x
     unset CRITICAL
 }
@@ -129,7 +135,7 @@ function backup_incremental() {
 function backup() {
     CRITICAL="$2/$(basename $1)"
     [ $DEBUG ] && set -x
-    btrfs $(logvv "-v") send "$1" | btrfs $(logvv "-v") receive "$2"
+    btrfs $(logvv "-v") send "$1" | btrfs $(logvv "-v") receive "$2" # todo indent output
     [ $DEBUG ] && set +x
     unset CRITICAL
 }
@@ -137,24 +143,25 @@ function backup() {
 function sync_subv_deletion() {
     local subdir
 
-    [ $DELETE ] && log "$(tput bold)${subv}$(tput sgr0) Syncing deletion of deleted timeshift backups..."
-    ! [ $DELETE ] && logv "$(tput bold)${subv}$(tput sgr0) Syncing deletion of readonly backups at destination only..."
+    [ $DELETE ] && log "$(tput bold)${subv}$(tput sgr0) Syncing deletion of deleted timeshift backups:"
+    ! [ $DELETE ] && logv "$(tput bold)${subv}$(tput sgr0) Syncing deletion of readonly backups at destination only:"
     for subdir in "$ROOT/../readonly/"*
     do
         if snapshot_deleted "$subdir" && [ -d "$subdir/$subv" ]
         then
-            logv "  Deleting $(basename $subdir)..."
-            btrfs subvolume delete "$subdir/$subv"
-            [ $DELETE ] && btrfs subvolume delete "$SYNC_DEST/snapshots/$(basename $subdir)/$subv"
+            logv "Deleting $(basename $subdir)..." | indent
+            btrfs subvolume delete "$subdir/$subv" | indent
+            [ $DELETE ] && btrfs subvolume delete "$SYNC_DEST/snapshots/$(basename $subdir)/$subv" | indent
         fi
     done
+    log " "
 }
 
 function sync_subv() {
     local subdir
     local past_subdir
 
-    log "$(tput bold)${subv}$(tput sgr0) Syncing timeshift backups..."
+    log "$(tput bold)${subv}$(tput sgr0) Syncing timeshift backups:"
 
     # iterate over all subdirs of $ROOT to find the $subvol
     for subdir in "$ROOT/"*
@@ -169,7 +176,7 @@ function sync_subv() {
         local readonly_subdir="$ROOT/../readonly/$(basename $subdir)"
         if [ -d "$SYNC_DEST/snapshots/$(basename $subdir)/$subv" ] && [ -d "$readonly_subdir/$subv" ]
         then
-            logv "  Skipping already synced snapshot '$(basename $subdir)'"
+            logv "Skipping already synced snapshot '$(basename $subdir)'" | indent
             past_subdir="$readonly_subdir"
             continue
         fi
@@ -177,27 +184,27 @@ function sync_subv() {
         # readonly subv exists
         if [ -d "$readonly_subdir/$subv" ]
         then
-            logvv "  Readonly snapshot '$(basename $subdir)' already exists."
+            logvv "Readonly snapshot '$(basename $subdir)' already exists." | indent
         else
-            logv "  Creating readonly snapshot of '$(basename $subdir)'"
+            logv "Creating readonly snapshot of '$(basename $subdir)'" | indent
             mkdir -p "$readonly_subdir"
 
             CRITICAL="$readonly_subdir/$subv"
-            btrfs subvolume snapshot -r "$subdir/$subv" "$readonly_subdir/$subv"
+            btrfs subvolume snapshot -r "$subdir/$subv" "$readonly_subdir/$subv" | indent
             unset CRITICAL
         fi
 
         # if readonly is synced
         if [ -d "$SYNC_DEST/snapshots/$(basename $subdir)/$subv" ]
         then
-            logvv "  Readonly already synced"
+            logvv "Readonly already synced" | indent
         else
-            log "  Syncing '$readonly_subdir' to '$SYNC_DEST'..."
+            log "Syncing '$readonly_subdir' to '$SYNC_DEST'..." | indent
             mkdir -p "$SYNC_DEST/snapshots/$(basename $subdir)"
 
             if [ -d "$past_subdir/$subv" ]
             then
-                logv "  Creating incremental backup of $(basename $subdir)"
+                logv "Creating incremental backup of $(basename $subdir)" | indent
                 backup_incremental "$past_subdir/$subv" "$readonly_subdir/$subv" "$SYNC_DEST/snapshots/$(basename $subdir)"
             else
                 backup "$readonly_subdir/$subv" "$SYNC_DEST/snapshots/$(basename $subdir)"
@@ -206,19 +213,20 @@ function sync_subv() {
 
         past_subdir="$readonly_subdir"
     done
+    log " "
 }
 
 function cleanup() {
     local subdir
-    log "Cleaning up left over directorys..."
+    log "Cleaning up left over directorys:"
 
     for subdir in "$ROOT/../readonly/"*
     do
         if snapshot_deleted "$subdir"
         then
-            logv "  Deleting $(basename $subdir)..."
-            rmdir $subdir
-            rmdir "$SYNC_DEST/snapshots/$(basename $subdir)"
+            logv "Deleting $(basename $subdir)" | indent
+            rmdir $subdir | indent
+            rmdir "$SYNC_DEST/snapshots/$(basename $subdir)" | indent
         fi
     done
 }
